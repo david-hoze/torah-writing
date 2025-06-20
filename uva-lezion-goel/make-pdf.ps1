130dec6c8044069c7f8e6578fdfbaa9a29e14928
David Hoze <david.hoze1@gmail.com>
2025-06-20 12:42:39 +0300

    Made first heading become title

--- uva-lezion-goel/make-pdf.ps1
+++ uva-lezion-goel/make-pdf.ps1
@@ -2,6 +2,7 @@ pandoc uva-lezion-goel.md `
   --pdf-engine=xelatex `
   --number-sections=false `
   --template=template.tex `
+  --lua-filter=title-from-header.lua `
   -o uva-lezion-goel.pdf
 
 Start-Process uva-lezion-goel.pdf
