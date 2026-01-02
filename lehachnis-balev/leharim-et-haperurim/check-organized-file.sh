0c23d50db0365029ba887de83159226a87717c70
David Hoze <david.hoze1@gmail.com>
2026-01-02 13:52:23 +0200

    Refactor

---
+++ lehachnis-balev/leharim-et-haperurim/check-organized-file.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+
+./squeeze-new-lines.sh $1
+./squeeze-new-lines.sh $2
+
+echo lines, words, chars
+wc $1
+wc $2
+
+sed 's/[0-9]//g' $1 | sort > orig.md
+sed 's/[0-9]//g' $2 | sort > organized.md
+
+if diff -1 -B orig.md organized.md >/dev/null; then
+    echo Updating file
+    mv $2 $1
+fi
+
+rm orig.md organized.md
--- lehachnis-balev/leharim-et-haperurim/organize-files.sh
+++ lehachnis-balev/leharim-et-haperurim/organize-files.sh
@@ -7,34 +7,5 @@ organizedSrc="sources_output.md"
 
 python organize-footnotes.py $orig $origSrc
 
-./squeeze-new-lines.sh $orig
-./squeeze-new-lines.sh $organized
-
-echo lines, words, chars
-wc $orig
-wc $organized
-
-sed 's/[0-9]//g' $orig | sort > orig.md
-sed 's/[0-9]//g' $organized | sort > organized.md
-
-if diff -1 -B orig.md organized.md >/dev/null; then
-    echo Updating file
-    mv $organized $orig
-fi
-
-./squeeze-new-lines.sh $origSrc
-./squeeze-new-lines.sh $organizedSrc
-
-echo lines, words, chars
-wc $origSrc
-wc $organizedSrc
-
-sed 's/[0-9]//g' $origSrc | sort > orig-src.md
-sed 's/[0-9]//g' $organizedSrc | sort > organized-src.md
-
-if diff -1 -B orig-src.md organized-src.md >/dev/null; then
-    echo Updating source file
-    mv $organizedSrc $origSrc
-fi
-
-rm orig.md organized.md orig-src.md organized-src.md
+./check-organized-file.sh $orig $organized
+./check-organized-file.sh $origSrc $organizedSrc
