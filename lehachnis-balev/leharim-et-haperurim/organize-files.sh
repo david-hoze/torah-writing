de3811b69a1aec93157063ac5adc7b8cac130750
David Hoze <david.hoze1@gmail.com>
2026-01-02 13:38:59 +0200

    Added checking and updating to script

--- lehachnis-balev/leharim-et-haperurim/organize-files.sh
+++ lehachnis-balev/leharim-et-haperurim/organize-files.sh
@@ -3,6 +3,7 @@
 orig="leharim-et-haperurim.md"
 origSrc="leharim-et-haperurim-sources.md"
 organized="article_output.md"
+organizedSrc="sources_output.md"
 
 python organize-footnotes.py $orig $origSrc
 
@@ -12,4 +13,18 @@ python organize-footnotes.py $orig $origSrc
 sed 's/[0-9]//g' $orig | sort > orig.md
 sed 's/[0-9]//g' $organized | sort > organized.md
 
-diff orig.md organized.md
+if diff -1 -B orig.md organized.md >/dev/null; then
+    echo Updating file
+    mv $organized $orig
+fi
+
+./squeeze-new-lines.sh $origSrc
+./squeeze-new-lines.sh $organizedSrc
+
+sed 's/[0-9]//g' $origSrc | sort > orig-src.md
+sed 's/[0-9]//g' $organizedSrc | sort > organized-src.md
+
+if diff -1 -B orig-src.md organized-src.md >/dev/null; then
+    echo Updating source file
+    mv $organizedSrc $origSrc
+fi
