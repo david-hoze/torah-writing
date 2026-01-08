01cc2fd038d59c8539b495de8172586e2b5d0461
David Hoze <david.hoze1@gmail.com>
2026-01-08 12:40:14 +0200

    Improved script

--- scripts/test/test-organize-footnotes.sh
+++ scripts/test/test-organize-footnotes.sh
@@ -8,6 +8,8 @@ if diff -B -b article_output.md article.md >/dev/null; then
     echo unordered article test passed
 else
     echo unordered article test not passed
+    mv article_output.md article-failed.md
+    WinMergeU.exe article.md article-failed.md
 fi
 
 $1 sources_output.md
@@ -17,6 +19,8 @@ if diff -B -b article_output.md sources.md >/dev/null; then
     echo unordered sources test passed
 else
     echo unordered sources test not passed
+    mv article_output.md sources-failed.md
+    WinMergeU.exe sources.md sources-failed.md
 fi
 
 rm article_output.md sources_output.md
@@ -27,6 +31,8 @@ if diff -B -b citation-mismatch.md expected-citation-mismatch.md >/dev/null; the
     echo citation mismatch test passed
 else
     echo citation mismatch not passed
+    mv citation-mismatch.md citation-mismatch-failed.md
+    WinMergeU.exe expected-citation-mismatch.md citation-mismatch-failed.md
 fi
 
-rm article_output.md sources_output.md
+rm article_output.md sources_output.md citation-mismatch.md
