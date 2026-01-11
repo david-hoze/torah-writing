db4b6b03bb4196fe564e597bdb6742fa0f1253cb
David Hoze <david.hoze1@gmail.com>
2026-01-11 08:24:35 +0200

    Removed monkey patching

--- scripts/organize-footnotes.rb
+++ scripts/organize-footnotes.rb
@@ -27,66 +27,64 @@ ordered_footnotes = ordered_text.
   .gsub(/הערה (\d+)/) { "הערה #{mapping[$1.to_i]}" }
 
 
-class String
-  def remove_quotations_with_following_parentheses
-    non_text = /[^\p{L}\p{N}\p{M}]/
-    quotation_count = 0
-    i = 0
-    out = ""
-    debug = false
-    while i < self.length
-      puts "At position #{i}, char is '#{self[i]}', quotation_count is #{quotation_count}" if debug
-      if self[i] == '"' && (i == 0 || self[i-1] =~ non_text)
-        puts "Found opening quotation at #{i}" if debug
-        quotation_count = 1
-        j = i + 1
-        while j < self.length
-          if self[j] == '"' && self[j+1] =~ non_text
-            puts "Found closing quotation at #{j}" if debug
-            puts "quotation_count is #{quotation_count}" if debug
-            quotation_count -= 1
-            j += 1
-            next
-          end
-          if self[j] == '"' && self[j-1] =~ non_text
-            quotation_count += 1
-            j += 1
-            next
-          end
-          if quotation_count == 0
-            break
-          end
+def remove_quotations_with_following_parentheses(text)
+  non_text = /[^\p{L}\p{N}\p{M}]/
+  quotation_count = 0
+  i = 0
+  out = ""
+  debug = false
+  while i < text.length
+    puts "At position #{i}, char is '#{text[i]}', quotation_count is #{quotation_count}" if debug
+    if text[i] == '"' && (i == 0 || text[i-1] =~ non_text)
+      puts "Found opening quotation at #{i}" if debug
+      quotation_count = 1
+      j = i + 1
+      while j < text.length
+        if text[j] == '"' && text[j+1] =~ non_text
+          puts "Found closing quotation at #{j}" if debug
+          puts "quotation_count is #{quotation_count}" if debug
+          quotation_count -= 1
           j += 1
+          next
         end
-        puts "Matched quotation from #{i} to #{j}" if debug
-        puts self[i..j-1] if debug
-        puts "Matching text: '#{self[j..-1]}' for parentheses" if debug
-        match = self[j..-1].match(/^\s*\(([^)]+)\)/)
-        if match
-          puts "Also found following parentheses: '#{match[0]}'" if debug
-          puts "Checking if there are more than one citation inside" if debug
-          citations = handle_citation_group(match[1]).select { |c| MyHelpers.is_citation(c) }
-          if citations.count > 1
-            puts "More than one citation found, keeping rest of citations" if debug
-            out << "טקסט בשביל הציטוטים" << "(" << citations[1..-1].join(", ") << ")"
-          end
-          puts "Removed text: '#{self[i..(j + match[0].length - 1)]}'" if debug
-          i = j + match[0].length
-          puts "Removing quotation and following parentheses, moving to position #{i}" if debug
-        else
-          puts "No following parentheses, keeping quotation" if debug
-          out << self[i..j]
-          i = j + 1
+        if text[j] == '"' && text[j-1] =~ non_text
+          quotation_count += 1
+          j += 1
+          next
         end
-      else
-        out << self[i]
-        i += 1
+        if quotation_count == 0
+          break
+        end
+        j += 1
       end
+      puts "Matched quotation from #{i} to #{j}" if debug
+      puts text[i..j-1] if debug
+      puts "Matching text: '#{text[j..-1]}' for parentheses" if debug
+      match = text[j..-1].match(/^\s*\(([^)]+)\)/)
+      if match
+        puts "Also found following parentheses: '#{match[0]}'" if debug
+        puts "Checking if there are more than one citation inside" if debug
+        citations = handle_citation_group(match[1]).select { |c| MyHelpers.is_citation(c) }
+        if citations.count > 1
+          puts "More than one citation found, keeping rest of citations" if debug
+          out << "טקסט בשביל הציטוטים" << "(" << citations[1..-1].join(", ") << ")"
+        end
+        puts "Removed text: '#{text[i..(j + match[0].length - 1)]}'" if debug
+        i = j + match[0].length
+        puts "Removing quotation and following parentheses, moving to position #{i}" if debug
+      else
+        puts "No following parentheses, keeping quotation" if debug
+        out << text[i..j]
+        i = j + 1
+      end
+    else
+      out << text[i]
+      i += 1
     end
-    puts "Resulting text:" if debug
-    puts out if debug
-    out
   end
+  puts "Resulting text:" if debug
+  puts out if debug
+  out
 end
 
 # test_text = %Q{"You need to (capture): "this", text (because): "it's a well" balanced (quotes): 'with inner quotes'" (and also the parantheses following), but not capture this text, 'cause it's not a quote right? (even though it has parantheses following it). "Also, capture this, it's also quotes" (followed by parantheses).}
@@ -136,7 +134,7 @@ footnote_citations = ordered_footnotes
   .scan(/^\[\^(\d+)\]:(.*)$/)
   .reduce({}) do |acc, (num, content)|
     citations = content
-      .remove_quotations_with_following_parentheses()
+      .then { remove_quotations_with_following_parentheses(_1) }
       .scan(/\(([^)]+)\)/)
       .map { handle_citation_group(_1[0]) }
       .flatten
